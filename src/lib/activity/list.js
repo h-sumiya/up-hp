b1d43d4f59ed5d325f5c6dbb05d84ae5f37d03a3
h-sumiya <sumiya.main01@gmail.com>
2024-04-08 16:56:49 +0900

    update lib

---
+++ src/lib/activity/list.js
@@ -0,0 +1,7 @@
+export const activityList = [
+    {
+        title:"サイトを公開しました。",
+		description:"SOMAからUnbiased Programmingへの改名に伴い新たなWEBサイトを公開しました。",
+		href:"/activity/site"
+    },
+];
---
+++ src/lib/news/list.js
@@ -0,0 +1,18 @@
+export const newsList = [
+    {
+        "title":"2024年新歓パーティーのお知らせ",
+        "description":"4/13に新歓パーティーを開催します。",
+        "href":"/news/welcome2024"
+    }   ,
+    {
+        "title": "2024年フレマンのお知らせ",
+        "description":"外資系就活ドットコム様に協賛していただき、様々なイベントを開催します。",
+		"href":"/news/gsskt"
+    },
+    {
+        "title": "新歓パーティーのお知らせ",
+        "description":"新歓パーティーを開催します。",
+        "href":"/news/sinkan",
+    },
+];
+
